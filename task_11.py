5f282baf53ea930e292df769afb9741fe6486c9f
aleksebv <101460659+alekseybvv@users.noreply.github.com>
2025-01-15 19:13:44 +0300

    Update task_11.py

--- task_11.py
+++ task_11.py
@@ -1,33 +1,33 @@
 class Dessert:
     def __init__(self, name=None, calories=None):
-        self._name = name  
-        self._calories = calories
+        self.name = name
+        self.calories = calories
 
     def get_name(self):
-        return self._name
+        return self.name
 
     def set_name(self, name):
-        self._name = name
+        self.name = name
 
     def get_calories(self):
-        return self._calories
+        return self.calories
 
     def set_calories(self, calories):
-        self._calories = calories
+        self.calories = calories
 
     def is_healthy(self):
-        if self._calories is not None and self._calories < 200:
-            return True
+        if self.calories is not None:
+            try:
+                calories = float(self.calories)
+                if calories < 200:
+                    return True
+            except ValueError:
+                pass
         return False
 
     def is_delicious(self):
         return True
 
 
-dessert = Dessert("twix", 200)
 
-print("Название:", dessert.get_name())
-print("Калорийность:", dessert.get_calories())
 
-print(dessert.is_healthy())  
-print(dessert.is_delicious())  
