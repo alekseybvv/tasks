aabd60a13953f4a916344cf11b43e707fa253363
aleksebv <101460659+alekseybvv@users.noreply.github.com>
2025-01-15 17:01:52 +0300

    Update task_01.py

--- task_01.py
+++ task_01.py
@@ -1,14 +1,19 @@
 def is_palindrome(string):
-  if string is None:
-    return False
+    if string is None:
+        return False
     
-  string = str(string)
-  
-  string = string.lower().replace(" ", "").replace(",", "").replace("!", "").replace("--", "")
-  return string == string[::-1]
+    string = str(string)
+    
+    string = string.lower()
+    empty_string = ""
+    for char in string:
+        if char.isalnum():  
+            empty_string += char
+    
+    return empty_string == empty_string[::-1]
 
 print(is_palindrome("A man, a plan, a canal -- Panama"))  
-print(is_palindrome("Madam, I'm Adam!"))                
-print(is_palindrome(333))                              
-print(is_palindrome(None))                              
+print(is_palindrome("Madam, I'm Adam!"))                 
+print(is_palindrome(333))                                
+print(is_palindrome(None))                               
 print(is_palindrome("Abracadabra"))                      
