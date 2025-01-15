e1e5918fa047a56ea347bd2ce86def16b18e4ff9
aleksebv <101460659+alekseybvv@users.noreply.github.com>
2025-01-15 19:14:39 +0300

    Update task_12.py

--- task_12.py
+++ task_12.py
@@ -1,6 +1,6 @@
 class Dessert:
     def __init__(self, name=None, calories=None):
-        self._name = name  
+        self._name = name
         self._calories = calories
 
     def get_name(self):
@@ -16,39 +16,34 @@ class Dessert:
         self._calories = calories
 
     def is_healthy(self):
-        if self._calories is not None and self._calories < 200:
-            return True
+        if self._calories is not None:
+            try:
+                calories = float(self._calories)  
+                if calories < 200:
+                    return True
+            except (ValueError, TypeError): 
+                pass
         return False
 
     def is_delicious(self):
         return True
 
 
-# Класс JellyBean, расширяющий класс Dessert
 class JellyBean(Dessert):
     def __init__(self, name=None, calories=None, flavor=None):
-        super().__init__(name, calories)  # Инициализируем родительский класс
+        super().__init__(name, calories)
         self._flavor = flavor
 
-    # Геттер для flavor
     def get_flavor(self):
         return self._flavor
 
-    # Сеттер для flavor
     def set_flavor(self, flavor):
         self._flavor = flavor
 
-    # Переопределение метода is_delicious
     def is_delicious(self):
         if self._flavor == "black licorice":
             return False
         return True
 
 
-jellybean = JellyBean("Jelly Bean", 150, "black licorice")
 
-print("Название:", jellybean.get_name())
-print("Калорийность:", jellybean.get_calories())
-print("Вкус:", jellybean.get_flavor())
-print(jellybean.is_healthy())  # True
-print(jellybean.is_delicious())  # False
