fdcc954c53f9a07ada8cf1cc0e78a10e16dc2b70
aleksebv <101460659+alekseybvv@users.noreply.github.com>
2025-01-24 23:12:58 +0300

    Update task_06.py

--- task_06.py
+++ task_06.py
@@ -4,49 +4,36 @@ class WrongNumberOfPlayersError(Exception):
 class NoSuchStrategyError(Exception):
     pass
 
-class RockPaperScissors:
-    @staticmethod
-    def rps_game_winner(players):
-        if len(players) != 2:
-            raise WrongNumberOfPlayersError 
+def rps_game_winner(players):
+    if len(players) != 2:
+        raise WrongNumberOfPlayersError("WrongNumberOfPlayersError")
 
-        player1, action1 = players[0]
-        player2, action2 = players[1]
+    player1, action1 = players[0]
+    player2, action2 = players[1]
 
-        valid_actions = ['R', 'P', 'S']
+    valid_actions = ['R', 'P', 'S']
       
-        if action1 not in valid_actions or action2 not in valid_actions:
-            raise NoSuchStrategyError
+    if action1 not in valid_actions or action2 not in valid_actions:
+        raise NoSuchStrategyError("NoSuchStrategyError")
 
-        if action1 == action2:
-            return f'{player1} {action1}'
+    if action1 == action2:
+        return f'{player1} {action1}'
 
-        if (action1 == 'R' and action2 == 'S') or (action1 == 'S' and action2 == 'P') or (action1 == 'P' and action2 == 'R'):
-            return f'{player1} {action1}'
-        else:
-            return f'{player2} {action2}'
+    if (action1 == 'R' and action2 == 'S') or (action1 == 'S' and action2 == 'P') or (action1 == 'P' and action2 == 'R'):
+        return f'{player1} {action1}'
+    else:
+        return f'{player2} {action2}'
 
-
-try:
-    result = RockPaperScissors.rps_game_winner([['player1', 'P'], ['player2', 'S'], ['player3', 'S']])
-    print(result)
-except Exception as e:
-    print(e.__class__.__name__) 
 
 try:
-    result = RockPaperScissors.rps_game_winner([['player1', 'P'], ['player2', 'A']])
-    print(result)
-except Exception as e:
-    print(e.__class__.__name__)  
+    print(rps_game_winner([['player1', 'P'], ['player2', 'S'], ['player3', 'S']]))  
+except WrongNumberOfPlayersError as n:
+    print(n)
 
 try:
-    result = RockPaperScissors.rps_game_winner([['player1', 'P'], ['player2', 'S']])
-    print(result)
-except Exception as e:
-    print(e.__class__.__name__)
+    print(rps_game_winner([['player1', 'P'], ['player2', 'A']]))  
+except NoSuchStrategyError as s:
+    print(s)
 
-try:
-    result = RockPaperScissors.rps_game_winner([['player1', 'P'], ['player2', 'P']])
-    print(result)
-except Exception as e:
-    print(e.__class__.__name__)
+print(rps_game_winner([['player1', 'P'], ['player2', 'S']]))  
+print(rps_game_winner([['player1', 'P'], ['player2', 'P']]))  
